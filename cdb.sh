931316eda235926b9f9d5071214cd915c5fa1928
sehun-kim <rogarithm@gmail.com>
2023-01-31 15:04:51 +0900

    선언한 변수는 cdb 함수 안에서만 쓰는 지역 변수여도 상관없다

--- cdb.sh
+++ cdb.sh
@@ -1,37 +1,37 @@
 #!/usr/bin/env bash
-BOOKMARK_DIR='/Users/sehun/tools/assistant/.cd_bookmarks'
-USAGE='Usage: cdb [-c|-g|-d|-l] [bookmark]
--c: create bookmark
--g: goto bookmark
--d: delete bookmark
--l: list bookmarks'
-
 function cdb() {
+	local bookmark_dir='/Users/sehun/tools/assistant/.cd_bookmarks'
+	local usage='Usage: cdb [-c|-g|-d|-l] [bookmark]
+	-c: create bookmark
+	-g: goto bookmark
+	-d: delete bookmark
+	-l: list bookmarks'
 
-	if  [ ! -e ${BOOKMARK_DIR} ] ; then
-		mkdir ${BOOKMARK_DIR}
+
+	if  [ ! -e ${bookmark_dir} ] ; then
+		mkdir ${bookmark_dir}
 	fi
 
 	case $1 in
 		# create bookmark
 		-c) shift
-		if [ ! -f ${BOOKMARK_DIR}/$1 ] ; then
-			echo "cd `pwd`" > ${BOOKMARK_DIR}/"$1" ;
+		if [ ! -f ${bookmark_dir}/$1 ] ; then
+			echo "cd `pwd`" > ${bookmark_dir}/"$1" ;
 		else
 			echo "Try again! Looks like there is already a bookmark '$1'"
 		fi
 		;;
 		# delete bookmark
 		-d) shift
-		if [ -f ${BOOKMARK_DIR}/$1 ] ; then
-			rm ${BOOKMARK_DIR}/"$1" ;
+		if [ -f ${bookmark_dir}/$1 ] ; then
+			rm ${bookmark_dir}/"$1" ;
 		else
 			echo "Oops, forgot to specify the bookmark" ;
 		fi    
 		;;
 		# list bookmarks
 		-l) shift
-		cd ${BOOKMARK_DIR}/ ;
+		cd ${bookmark_dir}/ ;
 		for file in $(ls)
 		do
 			echo "${file}: $(cat ${file})"
@@ -39,12 +39,12 @@ function cdb() {
 		;;
 		# help
 		-h) shift
-		echo "$USAGE" ;
+		echo "$usage" ;
 		;;
 		# goto bookmark
 		*)
-		if [ -f ${BOOKMARK_DIR}/$1 ] ; then
-			source ${BOOKMARK_DIR}/"$1"
+		if [ -f ${bookmark_dir}/$1 ] ; then
+			source ${bookmark_dir}/"$1"
 		else
 			echo "Looks like your bookmark does not exist." ;
 		fi
