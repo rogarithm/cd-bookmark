af76103deefc6b8e03a6a3298a14cb2079048382
sehun-kim <rogarithm@gmail.com>
2023-08-12 19:35:54 +0900

    스펙을 다듬는다

--- bookmarker_spec.rb
+++ bookmarker_spec.rb
@@ -2,12 +2,12 @@ require_relative './bookmarker'
 
 describe "Bookmarker", "operations" do
   it "should make new database file if not exists"
-  it "should create bookmark if given option is -c" do
+  it "should create bookmark" do
     #bmk = Bookmarker.new
     #bmk.create('test_bookmark')
   end
   it "should throw an exception if a bookmark to create is already created"
-  it "should delete bookmark if given option is -d"
+  it "should delete bookmark"
   it "should throw an exception if you did not specify a bookmark to delete"
   it "should list bookmark satisfying regex" do
     bmk = Bookmarker.new
@@ -17,7 +17,7 @@ describe "Bookmarker", "operations" do
       bookmark.split(":").last.should match /\S+/
     end
   end
-  it "should show simple help document -h" do
+  it "should show simple help document" do
     bmk = Bookmarker.new
     bmk.show_help_msg.should == <<-END_OF_MSG
 Usage: cdb [-c|-g|-d|-l] [bookmark]
@@ -28,5 +28,5 @@ Usage: cdb [-c|-g|-d|-l] [bookmark]
     END_OF_MSG
   end
   it "should go to directory of given bookmark"
-  it "should throw an exceptioin if given bookmark you want to go doesn't exist"
+  it "should throw an exception if given bookmark you want to go doesn't exist"
 end
